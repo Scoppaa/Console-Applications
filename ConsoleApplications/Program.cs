db862c87b351e9e7e949cdc2566c0e86a1e5709c
Brandon Scoppa <scoppa.brandon@gmail.com>
2019-10-25 21:24:57 -0400

    Add project files.

---
+++ ConsoleApplications/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////=================================================
+            ////Application Name: Hello World
+            ////Description: Program that prints "Hello World!"
+            ////=================================================
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+            //////=================================================
+            //////Application Name: TwoFer
+            //////Description: Takes a name from user input and places into sentance
+            //////=================================================
+            //Console.WriteLine("Enter a name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("One for " + name + ", one for me");
+            //Console.ReadLine();
+
+            ////////=================================================
+            ////////Application Name: Leap
+            ////////Description: Takes a year as input and reports if its a leap year
+            ////////=================================================
+            //Console.WriteLine("Enter a year");
+            //int year = Convert.ToInt32(Console.ReadLine());
+            //if (year % 4 == 0)
+            //{
+            //    Console.WriteLine(year + " is a leap year");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(year + " is not a leap year");
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+}
