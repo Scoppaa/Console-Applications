063eb7966e0cb4b9c8d4e1d780b53e78bca44aaa
Brandon Scoppa <scoppa.brandon@gmail.com>
2019-11-04 16:04:39 -0500

    Added collatz conjecture program

--- ConsoleApplications/Program.cs
+++ ConsoleApplications/Program.cs
@@ -17,19 +17,19 @@ namespace ConsoleApplications
             //Console.WriteLine("Hello World!");
             //Console.ReadLine();
 
-            //////=================================================
-            //////Application Name: TwoFer
-            //////Description: Takes a name from user input and places into sentance
-            //////=================================================
+            ////=================================================
+            ////Application Name: TwoFer
+            ////Description: Takes a name from user input and places into sentance
+            ////=================================================
             //Console.WriteLine("Enter a name: ");
             //string name = Console.ReadLine();
             //Console.WriteLine("One for " + name + ", one for me");
             //Console.ReadLine();
 
-            ////////=================================================
-            ////////Application Name: Leap
-            ////////Description: Takes a year as input and reports if its a leap year
-            ////////=================================================
+            ////=================================================
+            ////Application Name: Leap
+            ////Description: Takes a year as input and reports if its a leap year
+            ////=================================================
             //Console.WriteLine("Enter a year");
             //int year = Convert.ToInt32(Console.ReadLine());
             //if (year % 4 == 0)
@@ -42,17 +42,37 @@ namespace ConsoleApplications
             //}
             //Console.ReadLine();
 
-            //////////=================================================
-            //////////Application Name: Resistor
-            //////////Description: Creates an array of colors and allows the user to search for the index of it in the array
-            //////////=================================================
+            ////=================================================
+            ////Application Name: Resistor
+            ////Description: Creates an array of colors and allows the user to search for the index of it in the array
+            ////=================================================
             //string[] resistor = new string[] { "Red", "Blue", "White", "Black", "Orange", "Green" };
             //Console.WriteLine("Enter a resistor color to return its numeric representation");
             //string color = Console.ReadLine();
             //int index = Array.FindIndex(resistor, x => x.Contains(color)); //Find index in the array of the color
             //Console.WriteLine("The numeric representation of " + color + " is: " + index);
             //Console.ReadLine();
-            
+
+            //=================================================
+            //Application Name: Collatz Conjecture
+            //Description: 
+            //=================================================
+            Console.WriteLine("Enter a number to generate the collatz sequence");
+            int number= Convert.ToInt32(Console.ReadLine());
+            while (number != 1)
+            {
+                if (number % 2 == 0)
+                {
+                    number = number / 2;
+                }
+                else if (number % 2 == 1)
+                {
+                    number = 3 * number + 1;
+                }
+                Console.WriteLine(number);
+            }
+                Console.ReadLine();
+
         }
     }
 }
